6c5d406497895cfe0b1cef08bff980a3663d8f0d
Ajay Brahmakshatriya <ajaybr@mit.edu>
2025-04-28 12:26:43 +0530

    Fixed bug with comparator

--- client/reflectile.js
+++ client/reflectile.js
@@ -662,7 +662,7 @@ function canonicalizeIsland(island) {
 		if (a[0] < b[0]) return -1;
 		if (a[0] > b[0]) return 1;
 		if (a[1] < b[1]) return -1;
-		if (a[1] > a[1]) return 1;
+		if (a[1] > b[1]) return 1;
 		return 0;
 	});
 	return island;	
